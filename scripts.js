8b4b2b0da069dccf53581c1ab1db942c973b4446
flashgun <flashgun@xboxstat.ru>
2025-08-30 13:36:45 +0000

    removed "stat avail since..."

--- scripts.js
+++ scripts.js
@@ -14,15 +14,6 @@ function report()	{
 	if(titleid === 0)
 		return;
 
-	fetch("api/getjson.php?f=getgameinfo&titleid=" + titleid)
-	.then(res => res.json())
-	.then(res => {
-
-		Object.keys(res[0][0]).forEach( k => d3.select("#"+k).text(res[0][0][k]));
-		d3.select("#timesince").text( (new Date( +res[1][0] * 1000)).toLocaleDateString());
-
-	});
-
 	Object.keys(graphs).forEach(t => drawtab(t));
 	timegraph();
 
